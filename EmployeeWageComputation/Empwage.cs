82909c51fb035ae19347a59d4d7589a9d0d68879
Mohanraj209 <mohanraj1715@gmail.com>
2022-02-15 00:12:56 +0530

    Program to check Employee is Present or Not

---
+++ EmployeeWageComputation/Empwage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageComputation
+{
+    internal class Empwage
+    {
+        public static void GetAttendance()
+        {
+            int IS_FULL_TIME = 1;
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IS_FULL_TIME)
+                Console.WriteLine("Employee is present");
+            else
+                Console.WriteLine("Employee is absent");
+        }
+    }
+}
